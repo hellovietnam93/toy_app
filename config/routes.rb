f933dc106d243ba93b6999edde39435293db2c2e
TrungNT <hellovietnam93@gmail.com>
2016-07-03 10:43:53 +0700

    Rails flavored ruby

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "static_pages/home"
+  root "static_pages#home"
 
-  get "static_pages/help"
+  get "/help", to: "static_pages#help"
+  get "/contact", to: "static_pages#contact"
 end
